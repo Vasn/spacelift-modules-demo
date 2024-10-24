f992302fae2504dea016ce146693920f522620af
Vasilis Ng Jun Quan <vasilisngjunquan@RAiDs-MacBook-Pro.local>
2024-10-24 11:08:49 +0800

    update vpc provider

--- vpc/main.tf
+++ vpc/main.tf
@@ -7,6 +7,20 @@ terraform {
   }
 }
 
+provider "aws" {
+  region  = "ap-southeast-1"
+  # profile = var.aws_profile_name
+
+  # default_tags {
+  #   tags = {
+  #     app_name = "${var.project_name}-app"
+  #     cloud    = "aws"
+  #     version  = "1.0.0"
+  #     owner    = var.project_owner
+  #   }
+  # }
+}
+
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr_block
   instance_tenancy     = "default"
