b53e62c7c68f6c3a50177d8f27499d4af6b2cfb2
Vasilis Ng Jun Quan <vasilisngjunquan@RAiDs-MacBook-Pro.local>
2024-10-25 12:45:30 +0800

    add subnet module

--- subnet/main.tf
+++ subnet/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.67.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
 resource "aws_subnet" "main" {
   for_each = var.subnets
 
