9f9bfd2b5828b9b6a2212bb19a2f2766d2ac850f
Vasilis Ng Jun Quan <vasilisngjunquan@RAiDs-MacBook-Pro.local>
2024-10-28 15:17:46 +0800

    Refactor hardcoded variables

--- subnet/tests/stack-with-vpc/main.tf
+++ subnet/tests/stack-with-vpc/main.tf
@@ -12,41 +12,21 @@ provider "aws" {
 }
 
 resource "aws_vpc" "main" {
-  cidr_block = "10.1.0.0/24"
+  cidr_block = var.aws_region
 }
 
 module "subnet" {
   source = "../../"
 
-  vpc_id = aws_vpc.main.id
-  subnets = {
-    "public-subnet-1a" = {
-      "cidr_block"              = "10.1.0.0/28",
-      "availability_zone"       = "ap-southeast-1a",
-      "map_public_ip_on_launch" = true,
-      "name"                    = "public-subnet-a",
-      "tier"                    = "public"
-    },
-    "public-subnet-1b" = {
-      "cidr_block"              = "10.1.0.16/28",
-      "availability_zone"       = "ap-southeast-1b",
-      "map_public_ip_on_launch" = true,
-      "name"                    = "public-subnet-b",
-      "tier"                    = "public"
-    },
-    "app-subnet-1a" = {
-      "cidr_block"              = "10.1.0.48/28",
-      "availability_zone"       = "ap-southeast-1a",
-      "map_public_ip_on_launch" = false,
-      "name"                    = "app-subnet-a",
-      "tier"                    = "private"
-    },
-    "app-subnet-1b" = {
-      "cidr_block"              = "10.1.0.64/28",
-      "availability_zone"       = "ap-southeast-1b",
-      "map_public_ip_on_launch" = false,
-      "name"                    = "app-subnet-b",
-      "tier"                    = "private"
-    }
-  }
+  vpc_id  = aws_vpc.main.id
+  subnets = var.subnets
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS region used for test"
+}
+
+variable "subnets" {
+  description = "Map of subnet objects to be created"
 }
