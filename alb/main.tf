32e283ec48c332b2da4ece15bce9803c2621a339
Vasilis Ng Jun Quan <vasilisngjunquan@gmail.com>
2024-11-01 10:10:11 +0800

    change default ALB listener action

--- alb/main.tf
+++ alb/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.67.0"
-    }
-  }
-}
-
 # load balancer
 resource "aws_lb" "main" {
   name                       = "${var.project_name}-alb"
@@ -65,12 +56,8 @@ resource "aws_lb_listener" "http" {
   protocol          = "HTTP"
 
   default_action {
-    type = "fixed-response"
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "Not Found"
-      status_code  = "404"
-    }
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web.arn
   }
 }
 
